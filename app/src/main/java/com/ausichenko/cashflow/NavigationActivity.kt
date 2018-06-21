5481f21cc43386df8aead8510b70f32e2b786163
Andrey Usichenko <usichenko.andrey@gmail.com>
2018-06-21 18:32:22 +0700

    Add Material comp.

--- app/src/main/java/com/ausichenko/cashflow/NavigationActivity.kt
+++ app/src/main/java/com/ausichenko/cashflow/NavigationActivity.kt
@@ -1,34 +1,19 @@
 package com.ausichenko.cashflow
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_navigation.*
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
+
+
 
 class NavigationActivity : AppCompatActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val bar = findViewById<BottomAppBar>(R.id.bar)
+        setSupportActionBar(bar)
     }
 }
